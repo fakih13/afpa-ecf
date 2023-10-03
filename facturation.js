25c764f34589d5795acd29633a388d397f1f4b29
fakih13 <madi.bch@gmail.com>
2023-10-03 10:27:19 +0200

    adresse facturation

--- facturation.js
+++ facturation.js
@@ -1,25 +1,42 @@
 let same = document.getElementById('same');
+let delivery = document.getElementById('delivery');
 const facturation = document.getElementById('facturation');
 let facturationCity = document.getElementById('facturationCity');
 let facturationPostal = document.getElementById('facturationPostal');
 let facturationAdresse = document.getElementById('facturationAdresse');
 function sameAdress() {
   if (same.checked == true) {
-    let shipAdresse = document.getElementById('ship');
-    let city = document.getElementById('city');
-    let postal = document.getElementById('postal');
-    facturationAdresse.value = shipAdresse.value;
-    facturationCity.value = city.value;
-    facturationPostal.value = postal.value;
-    facturation.style.display = 'none';
-  } else {
-    facturation.style.display = 'flex';
+    facturation.style.display = 'flex'
+    // if (delivery) {
+    //   const inputs = delivery.getElementsByTagName('input');
+    //   const inputNonVides = true;
+    //   for (let i = 0; i < inputs.length; i++) {
+    //     let input = inputs[i];
+    //     if (input.value === '') {
+    //       alert("veuillez remplir tout les champs de l'adresse de livraison")
+    //       console.log(same.checked)
+    //     }else{
+    //       input.style.border = '2px solid green'
+    //     }
+    //   }
+    //   if (inputNonVides) {
+    //     let shipAdresse = document.getElementById('ship');
+    //     let city = document.getElementById('city');
+    //     let postal = document.getElementById('postal');
+    //     facturationAdresse.value = shipAdresse.value;
+    //     facturationCity.value = city.value;
+    //     facturationPostal.value = postal.value;
+    //     facturation.style.display = 'none';
+    //   } else {
+    //     facturation.style.display = 'flex';
+    //   }
+    // }
   }
 }
 
 const identification = document.getElementById('identificationContainer');
-const nom = document.getElementById('nom')
-const prenom =document.getElementById('prenom')
+const nom = document.getElementById('nom');
+const prenom = document.getElementById('prenom');
 
 const pro = document.getElementById('pro');
 
@@ -34,4 +51,4 @@ function proIdentification() {
 }
 
 same.addEventListener('change', sameAdress);
-society.addEventListener('change',proIdentification)
+society.addEventListener('change', proIdentification);
