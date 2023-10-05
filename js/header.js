813a99a2a2b843c8f018c761f57a81b2f01e4fef
fakih13 <madi.bch@gmail.com>
2023-10-05 16:50:21 +0200

    menu

--- js/header.js
+++ js/header.js
@@ -1,29 +1,32 @@
-const buttonMenu = document.getElementById('buttonMenu')
-const listMenu = document.getElementById('listMenu')
-const closeMenu = document.getElementById('closeMenu')
-const user = document.getElementById('userIcon')
+const buttonMenu = document.getElementById('buttonMenu');
+const listMenu = document.getElementById('listMenu');
+const closeMenu = document.getElementById('closeMenu');
+const user = document.getElementById('userIcon');
 
+if (document.getElementById('sousMenu')) {
+  const sousMenu = document.getElementById('sousMenu');
+  sousMenu.style.display = 'none';
+}
 
-buttonMenu.addEventListener('click', ()=>{
-    listMenu.style.display = "block"
-})
+buttonMenu.addEventListener('click', () => {
+  listMenu.style.display = 'block';
+});
 
-closeMenu.addEventListener('click', ()=>{
-    listMenu.style.display = "none"
-})
+closeMenu.addEventListener('click', () => {
+  listMenu.style.display = 'none';
+});
 
-const leMenu = document.getElementById('leMenu')
+const leMenu = document.getElementById('leMenu');
 
-leMenu.addEventListener('click',()=>{
-    console.log(leMenu.children)
-})
+leMenu.addEventListener('click', () => {
+  console.log(leMenu.children);
+});
 
-
-if (window.sessionStorage.getItem('connect')){
-    const deconnexion = document.getElementById('deconnexion')
-    deconnexion.style.display ="inline"
-    deconnexion.addEventListener('click', ()=>{
-        window.sessionStorage.removeItem('connect')
-        window.location.href = '/index.html'
-    })
+if (window.sessionStorage.getItem('connect')) {
+  const deconnexion = document.getElementById('deconnexion');
+  deconnexion.style.display = 'inline';
+  deconnexion.addEventListener('click', () => {
+    window.sessionStorage.removeItem('connect');
+    window.location.href = '/index.html';
+  });
 }
